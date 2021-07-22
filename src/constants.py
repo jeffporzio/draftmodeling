280f83dffadd6765347f7563e5160365e7abbd8b
jeffporzio <50245952+jeffporzio@users.noreply.github.com>
2021-07-22 17:31:45 -0400

    Reworked classes for batter and pitchers

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,17 @@
-POSITIONS = ["CATCHER","FIRST_BASE","SECOND_BASE","SHORTSTOP","THIRD_BASE","OUTFIELD","UTIL","BENCH", "INIT"]
+from enum import Enum
+
+# TODO: Are these different for daily?
+class Positions(Enum):
+    PITCHER = "PITCHER"
+    CATCHER = "CATCHER"
+    FIRST_BASE = "FIRST_BASE" 
+    SECOND_BASE = "SECOND_BASE"
+    SHORTSTOP = "SHORTSTOP"
+    THIRD_BASE = "THIRD_BASE"
+    OUTFIELD = "OUTFIELD"
+    UTIL = "UTIL"
+    BENCH = "BENCH"
+    INIT = "INIT"
 
 POSITION_REQS = {"CATCHER":1,
                 "FIRST_BASE":1,
@@ -19,3 +32,41 @@ NUM_TEAMS = 11
 
 SEASON_START_MONTH_DAY = '03-15'
 SEASON_END_MONTH_DAY = '10-15'
+
+
+"""
+Scoring Notes
+- Hitting statistics for Pitchers will not be counted, and Pitching statistics for Hitters will not be counted.
+- Pitchers will be scored based on their listed position on DraftKings. I.e. a pitcher who is listed as a starting pitcher (SP) on DraftKings will be scored as a starting pitcher even if he does not end up starting the game.
+- Holds will be scored using MLB's official definition of a Hold.
+"""
+class  dailyHitterPoints(Enum): 
+    SINGLE = 3
+    DOUBLE = 5
+    TRIPLE = 8 
+    HOMERUN = 10
+    RUN_BATTED_IN = 2
+    RUN = 2
+    BASE_ON_BALLS = 2
+    HIT_BY_PITCH = 2
+    SACRIFICE_FLY = 1.25
+    SACRIFICE_HIT = 1.25
+    STOLEN_BASE = 5
+
+class dailyPitcherPoints(Enum):
+    INNING_PITCHED_SP = 2.25
+    INNING_PITCHED_RP = 3.9 
+    STRIKEOUT_SP = 2
+    STRIKEOUT_RP = 3    
+    WIN = 4
+    EARNED_RUN_ALLOWED = -2
+    HIT_AGGAINST = -0.6
+    BASE_ON_BALLS_AGAINST = -0.6 
+    HIT_BATSMAN = -0.6
+    HOLD = 2.5
+    SAVE = 5
+    COMPLETE_GAME = 2.5 
+    COMPLETE_GAME_SHUTOUT = 2.5
+    NO_HITTER = 5
+    TEN_OR_MORE_STRIKEOUTS = 2
+    SEVEN_OR_MORE_INNINGS_PITCHED = 1.25
--- src/player.py
+++ src/player.py
@@ -1,29 +1,223 @@
+from abc import ABC
+from typing import Union
+
+from constants import Positions
+
+class Stats(ABC):
+    def __repr__(self): 
+        output: str = ""
+        temp = vars(self)
+        for item in temp:
+            output += f"{item}  :  {temp[item]}\n\t"
+        return output[:-2]
+
+class BatterStats(Stats): 
+    def __init__(self):
+        self.singles: int
+        self.doubles: int
+        self.triples: int
+        self.homeruns: int
+        self.ruslBattedIn: int
+        self.runs : int
+        self.baseOnBalls: int 
+        self.hitByPitch: int
+        self.sacrificeFlies: int
+        self.sacrificeHits: int
+        self.stolenBases: int
 
 
-# Hitters
-class Player(object): 
+class BatterStatsBuilder(object): 
+    def __init__(self): 
+        self.batterStats: BatterStats = BatterStats()
 
-    def __init__(self, name, id, runs, rbi, homeruns, stolen_bases, batting_avg, strike_outs, positions_allowed):
-        
+    def build(self)  -> BatterStats:
+        return self.batterStats
+    
+    def setSingles(self, input):
+        self.batterStats.singles = input
+        return self
+
+    def setDoubles(self, input):
+        self.batterStats.doubles = input
+        return self
+
+    def setTriples(self, input):
+        self.batterStats.triples = input
+        return self
+
+    def setHomeruns(self, input):
+        self.batterStats.homeruns = input
+        return self
+
+    def setRunsBattedIn(self, input):
+        self.batterStats.runsBattedIn = input
+        return self
+
+    def setRuns(self, input):
+        self.batterStats.runs = input
+        return self
+
+    def setBaseOnBalls(self, input):
+        self.batterStats.baseOnBalls = input
+        return self
+
+    def setHitByPitch(self, input):
+        self.batterStats.hitByPitch = input
+        return self
+
+    def setSacrificeFlies(self, input):
+        self.batterStats.sacrificeFlies = input
+        return self
+
+    def setSacrificeHits(self, input):
+        self.batterStats.sacrificeHits = input
+        return self
+
+    def setStolenBases(self, input):
+        self.batterStats.stolenBases = input
+        return self
+
+
+class PitcherStats(Stats):
+    def __init__(self):
+        self.isStartingPitcher: bool
+        self.inningsPitched: int
+        self.strikeouts: int
+        self.didWin: bool
+        self.earnedRunAllowed: int
+        self.hitsAgainst: int
+        self.baseOnBallsAgainst: int 
+        self.hitsBatsman: int
+        self.holds: int
+        self.saves: int
+        self.didCompleteGame: bool
+        self.didNoHitter: bool
+        self.didCompleteGameShutout:  bool
+        self.didTenOrMoreStrikeOuts: bool
+        self.didSevenOrMoreInningsPitched: bool
+
+class PitcherStatsBuilder: 
+    def __init__(self):
+        self.pitcherStats: PitcherStats = PitcherStats()
+
+    def setIsStartingPitcher(self, input):
+        self.pitcherStats.isStartingPitcher = input
+        return self
+
+    def setInningsPitched(self, input):
+        self.pitcherStats.inningsPitched = input
+        return self
+
+    def setStrikeouts(self, input):
+        self.pitcherStats.strikeouts = input
+        return self
+
+    def setDidWin(self, input):
+        self.pitcherStats.didWin = input
+        return self
+
+    def setEarnedRunAllowed(self, input):
+        self.pitcherStats.earnedRunAllowed = input
+        return self
+
+    def setHitsAgainst(self, input):
+        self.pitcherStats.hitsAgainst = input
+        return self
+
+    def setBaseOnBallsAgainst(self, input):
+        self.pitcherStats.baseOnBallsAgainst = input
+        return self
+
+    def setHitsBatsman(self, input):
+        self.pitcherStats.hitsBatsman = input
+        return self
+
+    def setHolds(self, input):
+        self.pitcherStats.holds = input
+        return self
+
+    def setSaves(self, input):
+        self.pitcherStats.saves = input
+        return self
+
+    def setDidCompleteGame(self, input):
+        self.pitcherStats.didCompleteGame = input
+        return self
+
+    def build(self) -> PitcherStats: 
+        # TODO: Check with Mike on logic
+        self.pitcherStats.didNoHitter = self.pitcherStats.hitsAgainst == 0
+        self.pitcherStats.didCompleteGameShutout = self.pitcherStats.didCompleteGame and self.pitcherStats.didNoHitter
+        self.pitcherStats.didSevenOrMoreInningsPitched = self.pitcherStats.inningsPitched >= 7
+        self.pitcherStats.didTenOrMoreStrikeOuts = self.pitcherStats.strikeouts >= 10
+        return self.pitcherStats
+
+
+class Player(object):
+
+    def __init__(self, name: str, id: int):
         self.name = name
         self.id = id
-        self.runs = runs 
-        self.rbi = rbi
-        self.homeruns = homeruns 
-        self.stolen_bases = stolen_bases
-        self.batting_avg = batting_avg
-        self.strike_outs = strike_outs
-        self.positions_allowed = positions_allowed
-        self.position = "INIT"
+        self.stats: Union[BatterStats, PitcherStats]
+        self.position: str = Positions.INIT
 
-    def disp(self): 
-        print( "Name: ", self.name)
-        print( "Runs: ", self.runs)
-        print( "RBIs: ", self.rbi)
-        print( "Homeruns: ", self.homeruns)
-        print( "Stolen bases: ", self.stolen_bases)
-        print( "Batting Avg: ", self.batting_avg)
-        print( "Strike outs: ", self.strike_outs)
-        print( "Position: ", self.position)
-        print( "Positions allowed: ", self.positions_allowed)
-        print()
+    def __repr__(self): 
+        return f"""
+        Name: {self.name}
+        Id: {self.id}
+        {repr( self.stats )}
+        Position: {self.position}
+        """
+class Batter(Player):
+    def __init__(self, name: str, id: int, batterStats: BatterStats, position: str):
+        Player.__init__(self, name, id)
+        self.stats = batterStats
+        self.position = position
+
+
+class Pitcher(Player):
+    def __init__(self, name: str, id: int, pitcherStats: PitcherStats, position: str):
+        Player.__init__(self, name, id)
+        self.stats = pitcherStats
+        self.position = position
+
+
+
+
+if __name__ == "__main__":
+    batterStatsBuilder: BatterStatsBuilder = BatterStatsBuilder()
+    batterStats: BatterStats = batterStatsBuilder \
+        .setSingles(1) \
+        .setDoubles(2) \
+        .setTriples(3) \
+        .setHomeruns(4) \
+        .setRunsBattedIn(5) \
+        .setRuns(6) \
+        .setBaseOnBalls(7) \
+        .setHitByPitch(8) \
+        .setSacrificeFlies(9) \
+        .setSacrificeHits(10) \
+        .setStolenBases(11) \
+        .build() 
+
+    pitcherStatsBuilder: PitcherStatsBuilder = PitcherStatsBuilder()
+    pitcherStats: PitcherStats = pitcherStatsBuilder \
+        .setIsStartingPitcher(1) \
+        .setInningsPitched(2) \
+        .setStrikeouts(3) \
+        .setDidWin(4) \
+        .setEarnedRunAllowed(5) \
+        .setHitsAgainst(6) \
+        .setBaseOnBallsAgainst(7) \
+        .setHitsBatsman(8) \
+        .setHolds(9) \
+        .setSaves(10) \
+        .setDidCompleteGame(11) \
+        .build()
+
+    
+    batter: Batter = Batter("John Smith", 1, batterStats, Positions.OUTFIELD)
+    print(batter)
+
+    pitcher: Pitcher = Pitcher("Jane Doe", 2, pitcherStats, Positions.PITCHER)
+    print(pitcher)
