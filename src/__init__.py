c4db4ff5e996492fca72b7a24bd4fac8ad933bc2
Jeffrey Porzio <jeffporzio@gmail.com>
2021-07-31 23:46:32 -0400

    clean up

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
-from .getPlayerData import getDailyBatterData, getDailyPitcherData, getSeasonalBatterData, getSeasonalPitcherData
+from .getPlayerData import getDailyBatterData
+from .getPlayerData import getDailyPitcherData
+from .getPlayerData import getSeasonalBatterData
+from .getPlayerData import getSeasonalPitcherData
 from .player import Player
 from .playerpool import Playerpool
-from .team import Team
